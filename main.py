1869837b097b124100a02b48fbd61db934fe295a
JJ-in-Jersey <118015245+JJ-in-Jersey@users.noreply.github.com>
2022-11-21 17:27:46 -0500

    Velocity calculations checkpoint. Calculations work when run from main in loop. Launching from pool works. Returning results doesn't.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 import multiprocess as mp
 from route_objects import GpxRoute
 from velocity import VelocityJob
-#from elapsed_time import elapsed_time
+from elapsed_time import ElapsedTimeJob
 
 if __name__ == '__main__':
 
@@ -37,9 +37,7 @@ if __name__ == '__main__':
     mp.job_queue.join()
     for rn in route.route_nodes(): rn.velocity_array(mp.result_lookup[id(rn)])
 
-    # For each edge, calculate time to travel the length for each time in the velocity array of the start node
-    for eg in route.route_edges():
-        elapsed_time(eg)
+    ElapsedTimeJob(route.first_route_node().next_edge())
 
 
 
--- velocity.py
+++ velocity.py
@@ -59,7 +59,7 @@ class VelocityJob:
 
     def execute(self):
         if exists(self.__output_file):
-            print(f'+     {self.__intro} {self.__code} {self.__name} using data file')
+            print(f'+     {self.__intro} {self.__code} {self.__name} reading data file')
             return tuple([self.__id, np.fromfile(self.__output_file, dtype=np.half)])
         else:
             print(f'+     {self.__intro} {self.__code} {self.__name} velocity calculation starting', flush=True)
@@ -82,7 +82,7 @@ class VelocityJob:
             noaa_dataframe.rename(columns={'Date_Time (LST/LDT)': 'time', ' Event': 'event', ' Speed (knots)': 'velocity'}, inplace=True)
             noaa_dataframe = noaa_dataframe[(start <= noaa_dataframe['time']) & (noaa_dataframe['time'] <= end)]
             noaa_dataframe = noaa_dataframe.reset_index(drop=True)
-            noaa_dataframe.to_csv(Path(str(self.__p_dir.folder())+'/'+self.__code+'_dataframe.csv'), index=False)
+            noaa_dataframe.to_csv(Path(str(self.__p_dir)+'/'+self.__code+'_dataframe.csv'), index=False)
             x = noaa_dataframe['time'].apply(lambda time: time_to_index(start, time)).to_numpy()
             y = noaa_dataframe['velocity'].to_numpy()
             cs = CubicSpline(x, y)
@@ -91,7 +91,7 @@ class VelocityJob:
             return tuple([self.__id, result])
 
     def execute_callback(self, result):
-        print(f'-     {self.__intro} {self.__code} calculation {"SUCCESSFUL" if isinstance(result[1], np.ndarray) else "FAILED"}', flush=True)
+        print(f'-     {self.__intro} {self.__code} {"SUCCESSFUL" if isinstance(result[1], np.ndarray) else "FAILED"}', flush=True)
     def error_callback(self, result):
         print(f'!     {self.__intro} {self.__code} process has raised an error: {result}', flush=True)
 
