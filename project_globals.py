a1f2c5b79a2045a7150afd0202848c71d5bdaba2
JJ-in-Jersey <118015245+JJ-in-Jersey@users.noreply.github.com>
2022-12-02 17:13:42 -0500

    incorporating Savitzky-Golay filter to transit time arrays creating minima table is working, declumping is not

--- project_globals.py
+++ project_globals.py
@@ -6,7 +6,7 @@ from datetime import timedelta as td
 from warnings import filterwarnings as fw
 
 fw("ignore", message="The localize method is no longer necessary, as this time zone supports the fold attribute",)
-timestep = 15
+timestep = 240
 boat_speeds = [v for v in range(-9, -1, 2)]+[v for v in range(3, 10, 2)]
 def sign(value): return value/abs(value)
 def seconds(start, end): return int((end-start).total_seconds())
--- transit_time.py
+++ transit_time.py
@@ -11,8 +11,7 @@ class TransitTimeJob:
     def __init__(self, route, speed, environ, chart_yr, intro=''):
         self.__speed = speed
         self.__intro = intro
-        self.__tt_output_file = Path(str(environ.transit_time_folder())+'/TT_'+str(self.__speed)+'_array.npy')
-        self.__min_output_file = Path(str(environ.transit_time_folder())+'MIN_'+str(self.__speed)+'_array.npy')
+        self.__output_file = Path(str(environ.transit_time_folder())+'/TT_'+str(self.__speed)+'.csv')
         self.__start = chart_yr.first_day_minus_one()
         self.__end = chart_yr.last_day_plus_one()
         self.__seconds = seconds(self.__start, self.__end)
@@ -23,25 +22,19 @@ class TransitTimeJob:
             self.__speed_df[col_name] = re.elapsed_time_dataframe()[col_name]
 
     def execute(self):
-        if exists(self.__tt_output_file) and exists(self.__min_output_file):
+        if exists(self.__output_file):
             print(f'+     {self.__intro} Transit time ({self.__speed}) reading data file', flush=True)
-            # noinspection PyTypeChecker
-            tt = np.load(self.__tt_output_file)
-            # noinspection PyTypeChecker
-            tt_minima = np.load(self.__min_output_file)
-            return tuple([self.__speed, tuple([tt, tt_minima])])
+            tt_minima_df = pd.read_csv(self.__output_file, header='infer')
+            return tuple([self.__speed, tt_minima_df])
         else:
             print(f'+     {self.__intro} Transit time ({self.__speed}) calculation starting', flush=True)
-            tt = np.fromiter([total_transit_time(row, self.__speed_df) for row in range(0, self.__no_timesteps)], dtype=int)
-            tt_minima = minima(tt)
-            # noinspection PyTypeChecker
-            np.save(self.__tt_output_file, tt)
-            # noinspection PyTypeChecker
-            np.save(self.__min_output_file, tt_minima)
-            return tuple([self.__speed, tuple([tt, tt_minima])])
+            tt_minima_df = minima_table(np.fromiter([total_transit_time(row, self.__speed_df) for row in range(0, self.__no_timesteps)], dtype=int))
+
+            tt_minima_df.to_csv(self.__output_file)
+            return tuple([self.__speed, tt_minima_df])
 
     def execute_callback(self, result):
-        print(f'-     {self.__intro} {self.__speed} {"SUCCESSFUL" if isinstance(result[1], np.ndarray) else "FAILED"} {self.__no_timesteps}', flush=True)
+        print(f'-     {self.__intro} {self.__speed} {"SUCCESSFUL" if isinstance(result[1], pd.DataFrame) else "FAILED"} {self.__no_timesteps}', flush=True)
     def error_callback(self, result):
         print(f'!     {self.__intro} {self.__speed} process has raised an error: {result}', flush=True)
 
@@ -54,23 +47,37 @@ def total_transit_time(init_row, d_frame):
         row += val
     return tt
 
-def minima(transit_time_array):
-    minima_threshold = 0.01
-    tt_median = np.median(transit_time_array)
+def minima_table(transit_time_array):
+    minima_threshold = 0.125
+    savgol = savgol_filter(transit_time_array, 100, 1)
+    tt_median = np.median(savgol)
     tt_df = pd.DataFrame()
-    tt_df['gradient'] = np.gradient(savgol_filter(transit_time_array, 100, 1))
-    tt_df['zero_ish'] = True if abs(tt_df['gradient']) < minima_threshold else False
-    zero_ish = tt_df['zero_ish'].to_numpy()
-    minima_clumps = []
+    tt_df['tt'] = transit_time_array
+    tt_df['Savitzky-Golay'] = (savgol)
+    tt_df['gradient'] = np.gradient(savgol)
+    tt_df['gradient'] = tt_df['gradient'].abs()
+    tt_df['zero_ish'] = tt_df['gradient'].apply(lambda x: True if x < minima_threshold else False)
+    tt_df['minima'] = False
+    tt_df.to_csv('C:\\users\\bronfelj\\downloads\\ER\\'+str(tt_median)+'.csv')
+
+    # convert clumps into single best estimate of minima
     clump = []
-    for x in zero_ish:
-        if x[1] > tt_median:
+    minima_clumps = []
+    zero_ish = tt_df['zero_ish'].to_numpy()
+    print(len(zero_ish), len(savgol))
+
+    for index, tt in enumerate(savgol):
+        if zero_ish[index] and tt > tt_median:
+            print(f'index: {index} clump len: {len(clump)}')
             if len(clump):
-                minima_clumps.append(clump)
+                print(np.median(clump))
+                minima_clumps.append(np.median(clump))
                 clump = []
-        else:
-            clump.append(x[0])
-    return [np.median(c) for c in minima_clumps]
+        elif zero_ish[index] and tt < tt_median:
+            clump.append(index)
+    for index in [np.median(c) for c in minima_clumps]:
+        tt_df.loc[index,'minima'] = True
+    return tt_df.drop(columns=['gradient', 'zero_ish'])
 
 
 
