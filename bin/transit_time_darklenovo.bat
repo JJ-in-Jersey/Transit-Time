fa5825976bc78b74274d7b44b50438c90634ea9c
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-07-08 23:25:19 -0400

    scripts update

---
+++ bin/transit_time_darklenovo.bat
@@ -0,0 +1,3 @@
+C:\Users\jason\PycharmProjects\Transit-Time\venv\Scripts\python.exe C:\Users\jason\PycharmProjects\Transit-Time\main.py "ER" "C:\users\jason\Developer Workspace\GPX\East River.gpx" 2023
+
+pause
