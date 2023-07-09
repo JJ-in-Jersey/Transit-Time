6de7e8bcd99d2c5cee161bd6ed317c1b3cc7b4a4
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-07-08 23:31:23 -0400

    scripts update

---
+++ bin/transit_time_dd_bms.bat
@@ -0,0 +1,3 @@
+C:\Users\bronfelj\PycharmProjects\Transit-Time\venv\Scripts\python.exe C:\Users\bronfelj\PycharmProjects\Transit-Time\main.py "ER" "C:\users\bronfelj\Developer Workspace\GPX\East River.gpx" 2023 -dd
+
+pause
