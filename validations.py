6807528b2473fc80181da2eb7b0e8eff740c5f86
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-08-26 23:23:18 -0400

    removing old code

--- validations.py
+++ validations.py
@@ -13,10 +13,6 @@ class HellGateSlackTimes:
         hell_gate = list(filter(lambda wp: not bool(wp.unique_name.find('Hell_Gate')), waypoints))[0]
         if ft.csv_npy_file_exists(hell_gate.interpolation_data_file):
             self.slack_df = ft.read_df(hell_gate.interpolation_data_file)
-            # first_event = self.hell_gate_df.at[0, 'Event']
-            # second_event = self.hell_gate_df.at[1, 'Event']
-            # first_slack = EastRiverValidation.first_slack_lookup[str(first_event+'_'+second_event)]
-            # print(f'{first_event}_{second_event} {first_slack}')
             self.slack_df = self.slack_df[self.slack_df['Event'] == 'slack'].copy()
             self.slack_df['date'] = self.slack_df['date_time'].apply(pd.to_datetime).dt.date
             self.slack_df['time'] = self.slack_df['date_time'].apply(pd.to_datetime).dt.time
