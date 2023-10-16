1425e62a49c6cdc110f02b1ae40b0730d7a9b547
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-10-15 21:09:34 -0400

    replacing multiprocessing module with job_manager package

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from argparse import ArgumentParser as argParser
 from pathlib import Path
-from multiprocessing import Manager
 
 import pandas as pd
 from time import perf_counter
@@ -9,12 +8,12 @@ from time import perf_counter
 from pandas import DataFrame, concat as Concat
 
 from tt_gpx.gpx import Route, Waypoint, Edge, TideWP, InterpolationWP, DataWP, CurrentStationWP
-from tt_semaphore import simple_semaphore as semaphore
+# from tt_semaphore import simple_semaphore as semaphore
 from tt_file_tools import file_tools as ft
 from tt_chrome_driver import chrome_driver
 from tt_date_time_tools import date_time_tools as dt
 
-import multiprocess as mpm
+# import multiprocess as mpm
 from velocity import CurrentStationJob, InterpolationJob, InterpolationDataJob
 from battery_validation import TideStationJob
 from elapsed_time import ElapsedTimeJob
@@ -23,6 +22,8 @@ from transit_time import TransitTimeMinimaJob
 from project_globals import TIMESTEP, TIME_RESOLUTION, WINDOW_MARGIN, boat_speeds, Environment, ChartYear
 
 from hell_gate_validation import HellGateSlackTimes
+
+from job_manager import JobManager
 
 checkmark = u'\N{check mark}'
 
@@ -57,10 +58,12 @@ if __name__ == '__main__':
 
     env.transit_time_folder.joinpath(str(route.elapsed_time_path.heading) + '.heading').touch()
 
-    mgr = Manager()
-    mpm.result_lookup = mgr.dict()
-    jm = mpm.WaitForProcess(target=mpm.JobManager, args=(mpm.job_queue, mpm.result_lookup))
-    jm.start()
+    job_manager = JobManager()
+
+    # mgr = Manager()
+    # mpm.result_lookup = mgr.dict()
+    # jm = mpm.WaitForProcess(target=mpm.JobManager, args=(mpm.job_queue, mpm.result_lookup))
+    # jm.start()
 
     chrome_driver.check_driver()
     if chrome_driver.latest_stable_version > chrome_driver.installed_driver_version:
@@ -71,24 +74,27 @@ if __name__ == '__main__':
     init_time = perf_counter()
     for wp in route.waypoints:
         if isinstance(wp, DataWP):  # DataWP must come before CurrentStationWP because DataWP IS A CurrentStationWP
-            mpm.job_queue.put(InterpolationDataJob(args['year'], wp))
+            job_manager.put(InterpolationDataJob(args['year'], wp))
             # idj = InterpolationDataJob(args['year'], wp)
             # idj.execute()
             # pass
         elif isinstance(wp, CurrentStationWP):
-            mpm.job_queue.put(CurrentStationJob(args['year'], wp, TIMESTEP, False))
+            job_manager.put(CurrentStationJob(args['year'], wp, TIMESTEP, False))
             # csj = CurrentStationJob(args['year'], wp, TIMESTEP)
             # csj.execute()
             # pass
-    mpm.job_queue.join()
+    job_manager.wait()
     print(f' Multi-process time {dt.mins_secs(perf_counter()-init_time)}')
 
     print(f'\nAdding results to waypoints', flush=True)
     for wp in route.waypoints:
         if isinstance(wp, CurrentStationWP) or isinstance(wp, DataWP):
-            wp.current_data = mpm.result_lookup[id(wp)]
+            wp.current_data = job_manager.get(id(wp))
             if isinstance(wp.current_data, pd.DataFrame): print(f'{checkmark}     {wp.unique_name}', flush=True)
             else: print(f'X     {wp.unique_name}', flush=True)
+
+    # del job_manager
+    # job_manager = JobManager()
 
     # Calculate the approximation of the velocity at interpolation points
     if len(route.interpolation_groups):
@@ -101,19 +107,19 @@ if __name__ == '__main__':
                 group_range = range(len(data_waypoints[0].current_data))
                 print(f'adding {len(group_range)} jobs to queue')
                 for i in group_range:
-                    mpm.job_queue.put(InterpolationJob(interpolation_pt, data_waypoints, len(group_range), i))  # (group, i, True) to display results
+                    job_manager.put(InterpolationJob(interpolation_pt, data_waypoints, len(group_range), i))  # (group, i, True) to display results
                     # ij = InterpolationJob(group, i)
                     # ij.execute()
-                mpm.job_queue.join()
+                job_manager.wait()
 
-                df = pd.DataFrame([mpm.result_lookup[str(id(interpolation_pt)) + '_' + str(i)] for i in group_range], columns=['date_index', 'velocity'])
+                df = pd.DataFrame([job_manager.get(str(id(interpolation_pt)) + '_' + str(i)) for i in group_range], columns=['date_index', 'velocity'])
                 interpolation_pt.set_current_data(df)
 
-            mpm.job_queue.put(CurrentStationJob(args['year'], interpolation_pt, TIMESTEP))
-            mpm.job_queue.join()
+            job_manager.put(CurrentStationJob(args['year'], interpolation_pt, TIMESTEP))
+            job_manager.wait()
 
             if isinstance(interpolation_pt, InterpolationWP):
-                interpolation_pt.current_data = mpm.result_lookup[id(interpolation_pt)]
+                interpolation_pt.current_data = job_manager.get(id(interpolation_pt))
                 if isinstance(interpolation_pt.current_data, pd.DataFrame): print(f'{checkmark}     {interpolation_pt.unique_name}', flush=True)
                 else: print(f'X     {interpolation_pt.unique_name}', flush=True)
         print(f'Multi-process time {dt.mins_secs(perf_counter()-init_time)}')
@@ -122,15 +128,15 @@ if __name__ == '__main__':
     print(f'\nCalculating elapsed times for edges (1st day-1 to last day+3)')
     init_time = perf_counter()
     for edge in route.elapsed_time_path.edges:
-        mpm.job_queue.put(ElapsedTimeJob(edge))
+        job_manager.put(ElapsedTimeJob(edge))
         # etj = ElapsedTimeJob(edge)
         # etj.execute()
-    mpm.job_queue.join()
+    job_manager.wait()
     print(f'Multi-process time {dt.mins_secs(perf_counter() - init_time)}')
 
     print(f'\nAdding results to edges')
     for edge in route.elapsed_time_path.edges:
-        edge.output_data = mpm.result_lookup[id(edge)]
+        edge.output_data = job_manager.get(id(edge))
         if isinstance(edge.output_data, DataFrame): print(f'{checkmark}     {edge.unique_name}', flush=True)
         else: print(f'X     {edge.unique_name}', flush=True)
 
@@ -142,20 +148,21 @@ if __name__ == '__main__':
     print(f'\nCalculating transit times (1st day-1 to last day+2)')
     init_time = perf_counter()
     for speed in boat_speeds:
-        mpm.job_queue.put(TransitTimeMinimaJob(env, cy, route, speed))
+        job_manager.put(TransitTimeMinimaJob(env, cy, route, speed))
     # tt = TransitTimeMinimaJob(env, cy, route, speed)
     # tt.execute()
-    mpm.job_queue.join()
+    job_manager.wait()
     print(f'Multi-process time {dt.mins_secs(perf_counter() - init_time)}')
 
     print(f'\nAdding transit time speed results to route')
     for speed in boat_speeds:
-        route.transit_time_lookup[speed] = mpm.result_lookup[speed]
+        route.transit_time_lookup[speed] = job_manager.get(speed)
         if isinstance(route.transit_time_lookup[speed], DataFrame): print(f'{checkmark}     tt {speed}', flush=True)
         else: print(f'X     tt {speed}', flush=True)
 
     if not ft.csv_npy_file_exists(env.transit_time_folder.joinpath('text_rotation')):
-        text_rotation_df = DataFrame(columns=['date', 'angle'])
+        # text_rotation_df = DataFrame(columns=['date', 'angle'])
+        text_rotation_df = pd.DataFrame({'date': pd.Series(dtype='datetime64[ns]'), 'angle': pd.Series(dtype='float')})
         text_arcs_df = Concat([route.transit_time_lookup[boat_speeds[-1]], route.transit_time_lookup[boat_speeds[-1]]])
         text_arcs_df.sort_values(['date', 'start'], ignore_index=True, inplace=True)
         for date in text_arcs_df['date'].drop_duplicates(ignore_index=True):
@@ -184,4 +191,4 @@ if __name__ == '__main__':
     battery_job.execute()
     ft.write_df(battery_job.battery_lines, env.transit_time_folder.joinpath('battery_tide'))
 
-    semaphore.off(mpm.job_manager_semaphore)
+    del job_manager
