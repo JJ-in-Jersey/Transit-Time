3bddaf395a59c51c7e03c3c2c06f7d83d04f4630
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-11-26 15:03:17 -0500

    separating downloading from calculating spline fit

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ from argparse import ArgumentParser as argParser
 from pathlib import Path
 
 import pandas as pd
-from time import perf_counter
 
 # noinspection PyPep8Naming
-from pandas import DataFrame, concat as Concat
+from pandas import concat as Concat
 
-from tt_gpx.gpx import Route, Waypoint, Edge, TideWP, InterpolatedDataWP, CurrentCalculationWP
+from tt_gpx.gpx import Route, Waypoint, Edge
+from tt_gpx.gpx import TideWP, InterpolatedDataWP, CurrentStationWP, InterpolatedWP
 from tt_file_tools import file_tools as ft
 from tt_chrome_driver import chrome_driver
-from tt_date_time_tools import date_time_tools as dt
 from tt_job_manager.job_manager import JobManager
 
 # import multiprocess as mpm
-from velocity import CurrentStationJob, InterpolationStationJob
-from velocity import InterpolatePointJob, InterpolationPointJob
+from velocity import NoaaCurrentDownloadJob, SplineFitCurrentDataJob, interpolate_group
 from battery_validation import TideStationJob
 from elapsed_time import ElapsedTimeJob
-from transit_time import TransitTimeMinimaJob
+from transit_time import TransitTimeJob
 from project_globals import TIMESTEP, TIME_RESOLUTION, WINDOW_MARGIN, boat_speeds, Environment, ChartYear
 
 from hell_gate_validation import HellGateSlackTimes
@@ -27,12 +25,17 @@ checkmark = u'\N{check mark}'
 
 if __name__ == '__main__':
 
+    # ---------- PARSE ARGUMENTS ----------
+
     ap = argParser()
     ap.add_argument('project_name', type=str, help='name of transit window project')
     ap.add_argument('filepath', type=Path, help='path to gpx file')
     ap.add_argument('year', type=int, help='calendar year for analysis')
     ap.add_argument('-dd', '--delete_data', action='store_true')
     args = vars(ap.parse_args())
+
+    # ---------- SET UP GLOBALS ----------
+
     env = Environment(args)
     cy = ChartYear(args)
 
@@ -40,7 +43,8 @@ if __name__ == '__main__':
     Waypoint.current_folder = env.current_folder
     Edge.elapsed_time_folder = env.elapsed_time_folder
 
-    # Assemble route
+    # ---------- ROUTE OBJECT ----------
+
     route = Route(args['filepath'], cy.edge_range())
 
     print(f'\nCalculating route "{route.filepath.stem}"')
@@ -56,93 +60,130 @@ if __name__ == '__main__':
 
     env.transit_time_folder.joinpath(str(route.elapsed_time_path.heading) + '.heading').touch()
 
+    # ---------- START MULTIPROCESSING ----------
+
     job_manager = JobManager()
+
+    # ---------- CHECK CHROME ----------
 
     chrome_driver.check_driver()
     if chrome_driver.latest_stable_version > chrome_driver.installed_driver_version:
         chrome_driver.install_stable_driver()
 
-    # Download noaa data and create velocity arrays for each CURRENT waypoint
-    print(f'\nDownloading and processing currents at CURRENT and INTERPOLATION DATA waypoints (1st day-1 to last day+4)', flush=True)
-    init_time = perf_counter()
-    for wp in route.waypoints:
-        if isinstance(wp, InterpolatedDataWP):  # DataWP must come before CurrentCalculationWP because DataWP IS A CurrentStationWP
-            job_manager.put(InterpolationStationJob(args['year'], wp))
-            # isj = InterpolationStationJob(args['year'], wp)
-            # result = isj.execute()
-        elif isinstance(wp, CurrentCalculationWP):
-            job_manager.put(CurrentStationJob(args['year'], wp, cy.waypoint_start_index(), cy.waypoint_end_index(), TIMESTEP))
-            # csj = CurrentStationJob(args['year'], wp, TIMESTEP)
-            # result = csj.execute()
-    job_manager.wait()
-    print(f' Multi-process time {dt.mins_secs(perf_counter()-init_time)}')
+    # ---------- INTERPOLATION WAYPOINTS ----------
 
-    print(f'\nAdding results to waypoints', flush=True)
+    print(f'\nDownloading current data for INTERPOLATED DATA WAYPOINTS (1st day-1 to last day+4)', flush=True)
     for wp in route.waypoints:
-        if isinstance(wp, CurrentCalculationWP) or isinstance(wp, InterpolatedDataWP):
-            wp.current_data = job_manager.get(id(wp))
+        if isinstance(wp, InterpolatedDataWP):
+            job_manager.put(NoaaCurrentDownloadJob(wp, cy.year(), cy.waypoint_start_index(), cy.waypoint_end_index()))
+    job_manager.wait()
+
+    print(f'\nAdding downloaded data to INTERPOLATED DATA WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, InterpolatedDataWP):
+            result = job_manager.get(id(wp))
+            wp.downloaded_current_data = result.dataframe
             print(f'{checkmark}     {wp.unique_name}', flush=True)
 
-    # Calculate the approximation of the velocity at each timestep of the interpolation waypoint
-    if len(route.interpolation_groups):
-        print(f'\nApproximating the velocity at INTERPOLATION waypoints (1st day-1 to last day+4)', flush=True)
-        for group in route.interpolation_groups:
-            interpolation_pt = group[0]
-            if not ft.csv_npy_file_exists(interpolation_pt.downloaded_data_filepath):
-                data_waypoints = group[1:]
-                group_range = range(len(data_waypoints[0].current_data))
-                print(f'adding {len(group_range)} jobs to queue')
-                init_time = perf_counter()
-                for i in group_range:
-                    job_manager.put(InterpolatePointJob(interpolation_pt, data_waypoints, len(group_range), i))  # (group, i, True) to display results
-                job_manager.wait()
-                print(f' Multi-process time {dt.mins_secs(perf_counter() - init_time)}')
+    print(f'\nSpline fitting data for INTERPOLATED DATA WAYPOINTS', flush=True)
+    # normalizing the time points for interpolation, don't want too many points, so using 3 hour timestep
+    for wp in route.waypoints:
+        if isinstance(wp, InterpolatedDataWP):
+            job_manager.put(SplineFitCurrentDataJob(wp, cy.waypoint_start_index(), cy.waypoint_end_index(), 10800))  # 3 hour timestep
+    job_manager.wait()
 
-                df = pd.DataFrame([job_manager.get(str(id(interpolation_pt)) + '_' + str(i)) for i in group_range], columns=['date_index', 'velocity'])
-                interpolation_pt.set_downloaded_data(df)
-            else:
-                interpolation_pt.set_downloaded_data(ft.read_df(interpolation_pt.downloaded_data_filepath))
+    print(f'\nAdding spline data to INTERPOLATED DATA WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, InterpolatedDataWP):
+            result = job_manager.get(id(wp))
+            wp.spline_fit_current_data = result.dataframe
+            print(f'{checkmark}     {wp.unique_name}', flush=True)
 
-            job_manager.put(InterpolationPointJob(args['year'], interpolation_pt, TIMESTEP))
+    print(f'\nInterpolating the data to approximate velocity for INTERPOLATED WAYPOINTS (1st day-1 to last day+4)', flush=True)
+    for group in route.interpolation_groups:
+        interpolate_group(group, job_manager)
+
+    print(f'\nSpline fit data from INTERPOLATED WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, InterpolatedWP):
+            job_manager.put(SplineFitCurrentDataJob(wp, cy.waypoint_start_index(), cy.waypoint_end_index()))
+    job_manager.wait()
+
+    print(f'\nAdding spline data to INTERPOLATED WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, InterpolatedWP):
+            result = job_manager.get(id(wp))
+            wp.spline_fit_current_data = result.dataframe
+            print(f'{checkmark}     {wp.unique_name}', flush=True)
+
+    # ---------- CURRENT STATION WAYPOINTS ----------
+
+    print(f'\nDownloading current data for CURRENT STATION WAYPOINTS (1st day-1 to last day+4)', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, CurrentStationWP):
+            job_manager.put(NoaaCurrentDownloadJob(wp, cy.year(), cy.waypoint_start_index(), cy.waypoint_end_index()))
+    job_manager.wait()
+
+    print(f'\nAdding downloaded data to CURRENT STATION WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, CurrentStationWP):
+            result = job_manager.get(id(wp))
+            wp.downloaded_current_data = result.dataframe
+            print(f'{checkmark}     {wp.unique_name}', flush=True)
+
+    print(f'\nSpline fit data from CURRENT STATION WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, CurrentStationWP):
+            job_manager.put(SplineFitCurrentDataJob(wp, cy.waypoint_start_index(), cy.waypoint_end_index()))
+    job_manager.wait()
+
+    print(f'\nAdding spline data to CURRENT STATION WAYPOINTS', flush=True)
+    for wp in route.waypoints:
+        if isinstance(wp, CurrentStationWP):
+            result = job_manager.get(id(wp))
+            wp.spline_fit_current_data = result.dataframe
+            print(f'{checkmark}     {wp.unique_name}', flush=True)
+
+    # ---------- ELAPSED TIME ----------
+
+    for s in boat_speeds:
+        speed_path = env.elapsed_time_folder.joinpath('elapsed_timesteps_'+str(s))
+        if ft.csv_npy_file_exists(speed_path):
+            elapsed_time_df = ft.read_df(speed_path)
+        else:
+            print(f'\nCalculating elapsed timesteps for edges at {s} kts (1st day-1 to last day+3)')
+            for edge in route.elapsed_time_path.edges:
+                job_manager.put(ElapsedTimeJob(edge, s))
             job_manager.wait()
 
-            interpolation_pt.current_data = job_manager.get(id(interpolation_pt))
-            print(f'{checkmark}     {interpolation_pt.unique_name}', flush=True)
-        print(f'Multi-process time {dt.mins_secs(perf_counter()-init_time)}')
-
-    # Calculate the number of timesteps to get from the start of the edge to the end of the edge
-    init_time = perf_counter()
-    for s in boat_speeds:
-        print(f'\nCalculating elapsed timesteps for edges {s} (1st day-1 to last day+3)')
-        for edge in route.elapsed_time_path.edges:
-            job_manager.put(ElapsedTimeJob(edge, s))
-        job_manager.wait()
-        print(f'Multi-process time {dt.mins_secs(perf_counter()-init_time)}')
-
-        print(f'\nAdding elapsed timesteps to dataframe', flush=True)
-        elapsed_time_df = pd.DataFrame(data={'departure_index': cy.edge_range()})  # add departure_index as the join column
-        for edge in route.elapsed_time_path.edges:
-            elapsed_time_df = elapsed_time_df.merge(job_manager.get(edge.unique_name + '_' + str(s)), on='departure_index')
-            print(f'{checkmark}     {edge.unique_name} {s}', flush=True)
-        print(f'\nAdding elapsed timesteps to route', flush=True)
-        elapsed_time_df.drop(['departure_index'], axis=1, inplace=True)
-        ft.write_df(elapsed_time_df, env.elapsed_time_folder.joinpath('elapsed_timesteps_'+str(s)))
+            print(f'\nAggregating elapsed timesteps at {s} kts into a dataframe', flush=True)
+            elapsed_time_df = pd.DataFrame(data={'departure_index': cy.edge_range()})  # add departure_index as the join column
+            for edge in route.elapsed_time_path.edges:
+                result = job_manager.get(edge.unique_name + '_' + str(s))
+                elapsed_time_df = elapsed_time_df.merge(result.dataframe, on='departure_index')
+                print(f'{checkmark}     {edge.unique_name} {s}', flush=True)
+            elapsed_time_df.drop(['departure_index'], axis=1, inplace=True)
+            ft.write_df(elapsed_time_df, speed_path)
+        print(f'\nAdding {s} kt dataframe to route', flush=True)
         route.elapsed_time_lookup[s] = elapsed_time_df
-    print(f'Multi-process time {dt.mins_secs(perf_counter() - init_time)}')
+
+    # ---------- TRANSIT TIMES ----------
 
     # calculate the number of timesteps from first node to last node
     print(f'\nCalculating transit timesteps (1st day-1 to last day+2)')
-    init_time = perf_counter()
     for speed in boat_speeds:
-        job_manager.put(TransitTimeMinimaJob(env, cy, route, speed))
+        f_date = cy.first_day.date()
+        l_date = cy.last_day.date()
+        tt_range = cy.transit_range()
+        tt_folder = env.transit_time_folder
+        job_manager.put(TransitTimeJob(speed, cy.year(), f_date, l_date, tt_range, route.elapsed_time_lookup[speed], tt_folder))
     job_manager.wait()
-    print(f'Multi-process time {dt.mins_secs(perf_counter() - init_time)}')
 
     print(f'\nAdding transit time speed results to route')
     for speed in boat_speeds:
-        route.transit_time_lookup[speed] = job_manager.get(speed)
-        if isinstance(route.transit_time_lookup[speed], DataFrame): print(f'{checkmark}     tt {speed}', flush=True)
-        else: print(f'X     tt {speed}', flush=True)
+        result = job_manager.get(speed)
+        route.transit_time_lookup[speed] = result.dataframe
+        print(f'{checkmark}     tt {speed}', flush=True)
 
     arcs_df = Concat([route.transit_time_lookup[key] for key in route.transit_time_lookup])
     arcs_df.sort_values(['date', 'name'], ignore_index=True, inplace=True)
