5d05393e2ef8129c785abb0a6314b50c9ce48954
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-12-01 14:28:20 -0500

    tide download

---
+++ tide.py
@@ -0,0 +1,80 @@
+import pandas as pd
+
+from tt_chrome_driver import chrome_driver as cd
+from tt_file_tools import file_tools as ft
+from tt_date_time_tools import date_time_tools as dtt
+from tt_job_manager.job_manager import Job
+
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+def dash_to_zero(value): return 0.0 if str(value).strip() == '-' else value
+
+
+class TideXMLDataframe:
+
+    def __init__(self, filepath):
+        tree = ft.XMLFile(filepath).tree
+
+        self.dataframe = pd.DataFrame(columns=['date', 'time', 'HL'])
+        for i in tree.find_all('item'):
+            self.dataframe.loc[len(self.dataframe)] = [i.find('date').text, i.find('time').text, i.find('highlow').text]
+
+        # need datetime so that when adding time, it can switch to the next day
+        self.dataframe['date_time'] = pd.to_datetime(self.dataframe['date'] + ' ' + self.dataframe['time'], format='%Y/%m/%d %H:%M')
+
+
+class DownloadedTideDataframe:
+
+    @staticmethod
+    def click_sequence(year, code):
+        code_string = '/noaatideannual.html?id=' + code
+        cd.WDW.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='" + code_string + "']"))).click()
+        dropdown = Select(cd.driver.find_element(By.ID, 'year'))
+        options = [int(o.text) for o in dropdown.options]
+        dropdown.select_by_index(options.index(year))
+        Select(cd.driver.find_element(By.ID, 'clock')).select_by_index(1)  # select 24 hour time
+        Select(cd.driver.find_element(By.ID, 'format')).select_by_index(2)
+
+    @staticmethod
+    def download_event():
+        cd.WDW.until(ec.element_to_be_clickable((By.ID, 'create_annual_tide_tables'))).click()
+
+    def __init__(self, year, download_path, folder, url, code, start_index, end_index):
+        self.dataframe = None
+
+        if ft.csv_npy_file_exists(download_path):
+            self.dataframe = ft.read_df(download_path)
+        else:
+            frame = pd.DataFrame()
+            cd.set_driver(folder)
+
+            for y in range(year - 1, year + 2):  # + 2 because of range behavior
+                cd.driver.get(url)
+                self.click_sequence(y, code)
+                downloaded_file = ft.wait_for_new_file(folder, self.download_event)
+                file_df = TideXMLDataframe(downloaded_file).dataframe
+                ft.write_df(frame, folder.joinpath(str(code) + '_' + str(y)))
+                frame = pd.concat([frame, file_df])
+
+            cd.driver.quit()
+
+            frame['date_index'] = frame['date_time'].apply(lambda x: dtt.int_timestamp(x))
+            frame = frame[(start_index <= frame['date_index']) & (frame['date_index'] <= end_index)]
+
+            ft.write_df(frame, download_path)
+            self.dataframe = frame
+
+
+class DownloadTideJob(Job):
+
+    def execute(self): return super().execute()
+    def execute_callback(self, result): return super().execute_callback(result)
+    def error_callback(self, result): return super().error_callback(result)
+
+    def __init__(self, waypoint, year, start, end):
+        result_key = id(waypoint)
+        arguments = tuple([year, waypoint.downloaded_path, waypoint.folder, waypoint.noaa_url, waypoint.code, start, end])
+        super().__init__(waypoint.unique_name, result_key, DownloadedTideDataframe, arguments)
