954a30c57c3f4d90493c8046aedf0059d60d9eaf
JJ-in-Jersey <jason.bronfeld@pm.me>
2023-12-01 15:11:05 -0500

    validations

--- bin/east_river_transit_time 2023.bat
+++ bin/east_river_transit_time 2023.bat
@@ -1,3 +1,3 @@
-python %userprofile%\PycharmProjects\Transit-Time\main.py "ER" "%userprofile%\Developer Workspace\GPX\East River.gpx" 2023
+python %userprofile%\PycharmProjects\Transit-Time\main.py "ER" "%userprofile%\Developer Workspace\GPX\East River.gpx" 2023 -hg -bat
 
 pause
