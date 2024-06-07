f61738fb0611cd6c6924d276e932e80a6ebcc945
JJ-in-Jersey <jason.bronfeld@pm.me>
2024-06-06 20:42:39 -0400

    FIS

---
+++ bin/fisher island sound 2024.bat
@@ -0,0 +1,3 @@
+python %userprofile%\PycharmProjects\Transit-Time\main.py "FIS" "%userprofile%\Developer Workspace\GPX\Fisher Island Sound.gpx" 2024
+
+pause
--- bin/fishers island sound 2024.bat
+++
@@ -1,3 +0,0 @@
-python %userprofile%\PycharmProjects\Transit-Time\main.py "FIS" "%userprofile%\Developer Workspace\GPX\Fishers Island Sound.gpx" 2024
-
-pause
